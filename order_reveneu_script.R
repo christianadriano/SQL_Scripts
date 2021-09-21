d8895681766776ac0ec75e48d387eb32890fae56
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-09-21 16:45:38 +0200

    done script

--- order_reveneu_script.R
+++ order_reveneu_script.R
@@ -1,7 +1,14 @@
 install.packages("sqldf")
 library(sqldf)
+library(tidyverse)
 
 #https://dept.stat.lsa.umich.edu/~jerrick/courses/stat701/notes/sql.html
 
 path <- "C://Users//Christian//Documents//GitHub//SQL_Scripts//data//"
-data <- read.csv(str_c(path, "order_channel_revenue.csv"))
+Data <- read.csv(str_c(path, "orders_channel_revenue.csv"))
+
+sqldf("SELECT * FROM Data")
+
+# Your result should return the following in a structured table:
+# Month | Channel | Avg. Revenue 
+sqldf("SELECT month,channel,AVG(revenue) AS average_reveneu FROM Data GROUP BY month, channel")
